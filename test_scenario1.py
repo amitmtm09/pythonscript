6f57c5cf0b271d9bb3b3a0c96475716d71ff4f6f
Amit <amitmtm09@gmail.com>
2020-06-18 22:55:24 +0530

    changes in Logs

--- test_scenario1.py
+++ test_scenario1.py
@@ -6,20 +6,20 @@ list1 = []
 driver = webdriver.Chrome(executable_path="F:\chromedriver_win32\chromedriver.exe")
 driver.get("https://www.rahulshettyacademy.com/seleniumPractise/#/")
 driver.maximize_window()
-driver.implicitly_wait(5)
 
 #SearchBox
 
 driver.find_element_by_css_selector("input[type='search']").send_keys("ber")
 time.sleep(5)
 driver.find_element_by_css_selector("button[type='submit']").click()
-
+time.sleep(5)
 #added all the item to cart
-
+time.sleep(5)
 products = driver.find_elements_by_xpath("//div/div/button[text()='ADD TO CART']") # add to cart
 #//div/div/button[text()='ADD TO CART']/parent::div/parent::div/h4
 for product in products:
     print(product.find_element_by_xpath("parent::div/parent::div/h4").text)
+    time.sleep(5)
     list1.append(product.find_element_by_xpath("parent::div/parent::div/h4").text)
     product.click()
 print(list1)
